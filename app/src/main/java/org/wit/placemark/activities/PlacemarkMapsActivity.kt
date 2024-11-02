2b24229705a8aa27b51385d45486c8ec758e4ada
Kate Molony <katemolony96@gmail.com>
2024-11-02 14:58:31 +0000

    Add exercise.

--- app/src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
@@ -7,6 +7,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.squareup.picasso.Picasso
 import org.wit.placemark.databinding.ActivityPlacemarkMapsBinding
 import org.wit.placemark.databinding.ContentPlacemarkMapsBinding
 import org.wit.placemark.main.MainApp
@@ -45,8 +46,12 @@ class PlacemarkMapsActivity : AppCompatActivity(), GoogleMap.OnMarkerClickListen
     }
 
     override fun onMarkerClick(marker: Marker): Boolean {
-        contentBinding.currentTitle.text = marker.title
-
+        //val placemark = marker.tag as PlacemarkModel
+        val tag = marker.tag as Long
+        val placemark = app.placemarks.findById(tag)
+        contentBinding.currentTitle.text = placemark!!.title
+        contentBinding.currentDescription.text = placemark.description
+        Picasso.get().load(placemark.image).into(contentBinding.currentImage)
         return false
     }
 
