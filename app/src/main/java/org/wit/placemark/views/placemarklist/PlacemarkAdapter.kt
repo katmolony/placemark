382cd14aae6e4d49830d0bef47ad57867851bbed
Kate Molony <katemolony96@gmail.com>
2024-11-02 20:06:00 +0000

    Add rating in list view.

--- app/src/main/java/org/wit/placemark/views/placemarklist/PlacemarkAdapter.kt
+++ app/src/main/java/org/wit/placemark/views/placemarklist/PlacemarkAdapter.kt
@@ -35,6 +35,7 @@ class PlacemarkAdapter constructor(private var placemarks: List<PlacemarkModel>,
         fun bind(placemark: PlacemarkModel, listener: PlacemarkListener) {
             binding.placemarkTitle.text = placemark.title
             binding.description.text = placemark.description
+            binding.rating.text = placemark.rating.toString()
             Picasso.get().load(placemark.image).resize(200,200).into(binding.imageIcon)
             binding.root.setOnClickListener { listener.onPlacemarkClick(placemark,adapterPosition) }
         }
